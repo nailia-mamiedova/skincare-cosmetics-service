300e8217b91fcd6f276a79dc4ebf33786c5f38aa
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-09-07 14:45:32 +0300

    Add cosmetics to INSTALLED_APPS & change TIME_ZONE to Europe/Kiev

--- skincare_service/settings.py
+++ skincare_service/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "cosmetics",
 ]
 
 MIDDLEWARE = [
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Kiev"
 
 USE_I18N = True
 
