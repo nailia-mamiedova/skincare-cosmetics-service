1b1ea08fe2cb7c54c26bd435bf170b078b99cd2c
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-09-08 12:35:27 +0300

    Add initial models: Brand, SkinType, Product, and custom Member user

--- cosmetics/models.py
+++ cosmetics/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Brand(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    country = models.CharField(max_length=255)
+
+
+class SkinType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    skin_types = models.ManyToManyField(SkinType, related_name="products")
+    ingredients = models.TextField()
+    description = models.TextField()
+
+
+class Member(AbstractUser):
+    date_of_birth = models.DateField(null=True)
+    skin_type = models.ForeignKey(SkinType, on_delete=models.CASCADE, null=True)
+    favorite_products = models.ManyToManyField(Product, related_name="members")
