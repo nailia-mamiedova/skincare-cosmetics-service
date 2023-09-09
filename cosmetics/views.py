014b06644e0fd5c89bb50f353948edf790fac323
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-09-09 12:37:23 +0300

    Initialize index view function & create empty index.html in cosmetics directory within templates

--- cosmetics/views.py
+++ cosmetics/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """View function for the home page of the site."""
+    return render(request, "cosmetics/index.html")
