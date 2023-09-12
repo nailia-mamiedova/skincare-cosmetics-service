1ca6c2397da4090d821e4101c1472b11b907a789
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-09-12 11:29:47 +0300

    Update index view with site statistics and visit count

--- cosmetics/views.py
+++ cosmetics/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from .models import Brand, Product, SkinType, Member
 
 
 def index(request):
     """View function for the home page of the site."""
-    return render(request, "cosmetics/index.html")
+
+    num_brands = Brand.objects.count()
+    num_products = Product.objects.count()
+    num_skin_types = SkinType.objects.count()
+    num_members = Member.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_brands": num_brands,
+        "num_products": num_products,
+        "num_skin_types": num_skin_types,
+        "num_members": num_members,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "cosmetics/index.html", context=context)
