5e8be1f73bd464384b2f5222b8eceb8f9332d506
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-09-21 18:29:09 +0300

    Add crispy forms to project settings

--- skincare_service/settings.py
+++ skincare_service/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
     "cosmetics",
 ]
 
@@ -68,6 +69,8 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 WSGI_APPLICATION = "skincare_service.wsgi.application"
 
