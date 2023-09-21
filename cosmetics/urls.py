28ada9ab0a7586059d67a0dd4415fe90e8725c04
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-09-21 17:00:14 +0300

    Add path to brands page to cosmetics/urls.py

--- cosmetics/urls.py
+++ cosmetics/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import index, restricted_view
-
+from .views import index, restricted_view, BrandListView
 
 urlpatterns = [
     path("", index, name="index"),
     path("restricted/", restricted_view, name="restricted"),
+    path("brands/", BrandListView.as_view(), name="brand-list"),
 ]
 
 app_name = "cosmetics"
