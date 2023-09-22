94bc94b50860d09f9609054bffd299784b4d7d67
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-09-22 16:46:15 +0300

    Add search form to products list for name-based queries

--- cosmetics/forms.py
+++ cosmetics/forms.py
@@ -8,3 +8,12 @@ class BrandSearchForm(forms.Form):
         label="",
         widget=forms.TextInput(attrs={"placeholder": "Search by name"})
     )
+
+
+class ProductSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by name"})
+    )
