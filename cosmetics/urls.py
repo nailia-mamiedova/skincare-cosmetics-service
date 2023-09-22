44419ed4fccb3f94bd12cf4069b68fdf77c4cfa6
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-09-22 19:49:28 +0300

    Add URL patterns for product creation, updation, and deletion in urls.py

--- cosmetics/urls.py
+++ cosmetics/urls.py
@@ -6,7 +6,10 @@ from .views import (
     BrandCreateView,
     BrandUpdateView,
     BrandDeleteView,
-    ProductListView
+    ProductListView,
+    ProductCreateView,
+    ProductUpdateView,
+    ProductDeleteView,
 )
 
 urlpatterns = [
@@ -17,6 +20,9 @@ urlpatterns = [
     path("brands/<int:pk>/update/", BrandUpdateView.as_view(), name="brand-update"),
     path("brands/<int:pk>/delete/", BrandDeleteView.as_view(), name="brand-delete"),
     path("products/", ProductListView.as_view(), name="product-list"),
+    path("products/create/", ProductCreateView.as_view(), name="product-create"),
+    path("products/<int:pk>/update/", ProductUpdateView.as_view(), name="product-update"),
+    path("products/<int:pk>/delete/", ProductDeleteView.as_view(), name="product-delete"),
 ]
 
 app_name = "cosmetics"
