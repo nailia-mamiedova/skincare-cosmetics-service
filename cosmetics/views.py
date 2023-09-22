bb18212438998d61dfe00bd1c84e10e948160c69
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-09-22 16:47:21 +0300

    Integrate search functionality in ProductListView for name-based filtering

--- cosmetics/views.py
+++ cosmetics/views.py
@@ -85,6 +85,28 @@ class ProductListView(LoginRequiredMixin, generic.ListView):
     template_name = "cosmetics/product_list.html"
     paginate_by = 8
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ProductListView, self).get_context_data(**kwargs)
+
+        name = self.request.GET.get("name", "")
+
+        context["search_form"] = ProductSearchForm(initial={
+            "name": name,
+        })
+
+        return context
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        form = ProductSearchForm(self.request.GET)
+
+        if form.is_valid():
+            return queryset.filter(
+                name__icontains=form.cleaned_data["name"],
+            )
+
+        return queryset
+
 
 def restricted_view(request):
     return render(request, 'cosmetics/restricted.html')
