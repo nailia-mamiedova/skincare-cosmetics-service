c56506aa249092a1288dfa3c13a6aa670b883bf1
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-09-22 16:56:32 +0300

    Add path to products page to cosmetics/urls.py

--- cosmetics/urls.py
+++ cosmetics/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
-from .views import index, restricted_view, BrandListView, BrandCreateView, BrandUpdateView, BrandDeleteView
+from .views import (
+    index,
+    restricted_view,
+    BrandListView,
+    BrandCreateView,
+    BrandUpdateView,
+    BrandDeleteView,
+    ProductListView
+)
 
 urlpatterns = [
     path("", index, name="index"),
@@ -8,6 +16,7 @@ urlpatterns = [
     path("brands/create/", BrandCreateView.as_view(), name="brand-create"),
     path("brands/<int:pk>/update/", BrandUpdateView.as_view(), name="brand-update"),
     path("brands/<int:pk>/delete/", BrandDeleteView.as_view(), name="brand-delete"),
+    path("products/", ProductListView.as_view(), name="product-list"),
 ]
 
 app_name = "cosmetics"
