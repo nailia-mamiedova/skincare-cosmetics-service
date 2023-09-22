9c9f694de8e42978ee09ca928eacb4f94215e885
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-09-22 15:09:18 +0300

    Add search form to brands list for name-based queries

---
+++ cosmetics/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+
+class BrandSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by name"})
+    )
