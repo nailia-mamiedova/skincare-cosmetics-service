2020993e56caea0f403a5277f0ebf68f3d53dbe2
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-09-22 14:05:57 +0300

    Add URL patterns for brand creation, updation, and deletion in urls.py

--- cosmetics/urls.py
+++ cosmetics/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import index, restricted_view, BrandListView
+from .views import index, restricted_view, BrandListView, BrandCreateView, BrandUpdateView, BrandDeleteView
 
 urlpatterns = [
     path("", index, name="index"),
     path("restricted/", restricted_view, name="restricted"),
     path("brands/", BrandListView.as_view(), name="brand-list"),
+    path("brands/create/", BrandCreateView.as_view(), name="brand-create"),
+    path("brands/<int:pk>/update/", BrandUpdateView.as_view(), name="brand-update"),
+    path("brands/<int:pk>/delete/", BrandDeleteView.as_view(), name="brand-delete"),
 ]
 
 app_name = "cosmetics"
