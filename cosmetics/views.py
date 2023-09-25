9add176fd58aff4919f60aefe340449214977937
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-09-25 17:33:16 +0300

    Integrate search functionality in SkinTypeListView for name-based filtering

--- cosmetics/views.py
+++ cosmetics/views.py
@@ -125,6 +125,28 @@ class SkinTypeListView(LoginRequiredMixin, generic.ListView):
     template_name = "cosmetics/skin_type_list.html"
     paginate_by = 4
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(SkinTypeListView, self).get_context_data(**kwargs)
+
+        name = self.request.GET.get("name", "")
+
+        context["search_form"] = SkinTypeSearchForm(initial={
+            "name": name,
+        })
+
+        return context
+
+    def get_queryset(self):
+        queryset = SkinType.objects.all()
+        form = SkinTypeSearchForm(self.request.GET)
+
+        if form.is_valid():
+            return queryset.filter(
+                name__icontains=form.cleaned_data["name"],
+            )
+
+        return queryset
+
 
 def restricted_view(request):
     return render(request, 'cosmetics/restricted.html')
