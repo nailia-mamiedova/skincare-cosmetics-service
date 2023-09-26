5a7decf6d290cf2c841e4adf96244e9832d6e62a
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-09-26 18:58:15 +0300

    Add date of birth validation function

--- cosmetics/forms.py
+++ cosmetics/forms.py
@@ -1,4 +1,17 @@
+from datetime import datetime
 from django import forms
+from django.core.exceptions import ValidationError
+
+
+def validate_date_of_birth(date_of_birth):
+    if date_of_birth.year < datetime.now().year - 99:
+        raise ValidationError("You cannot be that old")
+    elif date_of_birth.year > datetime.now().year:
+        raise ValidationError("You are not born yet")
+    elif date_of_birth.year > datetime.now().year - 18:
+        raise ValidationError("You should be at least 18 years old")
+
+    return date_of_birth
 
 
 class BrandSearchForm(forms.Form):
