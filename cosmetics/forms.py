3433c9dfaa8320ea1b183bb13bdc5ae246eed97a
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-09-26 18:45:19 +0300

    Add search form to member list for username-based queries

--- cosmetics/forms.py
+++ cosmetics/forms.py
@@ -26,3 +26,12 @@ class SkinTypeSearchForm(forms.Form):
         label="",
         widget=forms.TextInput(attrs={"placeholder": "Search by name"})
     )
+
+
+class MemberSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by username"})
+    )
