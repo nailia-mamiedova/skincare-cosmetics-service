ed26e66bdec91e5739997548bb10a821744d2a89
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-09-27 17:40:59 +0300

    Add form validation tests for member creation and update in 'test_forms'

---
+++ cosmetics/tests/test_forms.py
@@ -0,0 +1,37 @@
+from datetime import date
+from django.test import TestCase
+from cosmetics.forms import MemberCreationForm, MemberUpdateForm
+from cosmetics.models import SkinType
+
+
+class MemberFormTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.skin_type = SkinType.objects.create(name="Oily Skin")
+
+    def test_member_creation_form_is_valid(self):
+        form_data = {
+            "username": "test_user",
+            "password1": "test_password_12345",
+            "password2": "test_password_12345",
+            "first_name": "Test First",
+            "last_name": "Test Last",
+            "date_of_birth": date(2002, 2, 2),
+            "skin_type": self.skin_type.id,
+        }
+        form = MemberCreationForm(data=form_data)
+
+        self.assertTrue(form.is_valid())
+
+    def test_member_update_form_is_valid(self):
+        form_data = {
+            "username": "test_user",
+            "first_name": "Test First",
+            "last_name": "Test Last",
+            "date_of_birth": date(2002, 2, 2),
+            "skin_type": self.skin_type.id,
+        }
+
+        form = MemberUpdateForm(data=form_data)
+
+        self.assertTrue(form.is_valid())
