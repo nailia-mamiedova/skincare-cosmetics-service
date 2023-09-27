c8df0fcbca0d788029c8ac7b7e5a3298182e37d4
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-09-27 18:27:15 +0300

    Create a PrivateBrandListTest to check if logged-in users can access the brand list view

--- cosmetics/tests/test_view_brand.py
+++ cosmetics/tests/test_view_brand.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
+from cosmetics.models import Brand
 
 BRAND_LIST_URL = reverse("cosmetics:brand-list")
 
@@ -10,3 +12,17 @@ class PublicBrandListTest(TestCase):
         response = self.client.get(BRAND_LIST_URL)
 
         self.assertNotEquals(response.status_code, 200)
+
+
+class PrivateBrandListTest(TestCase):
+    def setUp(self) -> None:
+        self.user = get_user_model().objects.create_user(
+            username="user",
+            password="user12345",
+        )
+        self.client.force_login(self.user)
+
+    def test_login_required(self):
+        res = self.client.get(BRAND_LIST_URL)
+
+        self.assertEquals(res.status_code, 200)
