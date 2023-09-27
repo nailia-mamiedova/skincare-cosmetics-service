f24f607ec13360205fffc94a0b244245f74a7b07
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-09-27 18:35:45 +0300

    Create PrivateSkinTypeListTest and check if logged-in users can access the skin type list view

--- cosmetics/tests/test_view_skin_type.py
+++ cosmetics/tests/test_view_skin_type.py
@@ -1,5 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
+
+from cosmetics.models import SkinType
 
 SKIN_TYPE_LIST_URL = reverse("cosmetics:skin-type-list")
 
@@ -9,3 +12,17 @@ class PublicSkinTypeListTest(TestCase):
         response = self.client.get(SKIN_TYPE_LIST_URL)
 
         self.assertNotEquals(response.status_code, 200)
+
+
+class PrivateSkinTypeListTest(TestCase):
+    def setUp(self) -> None:
+        self.user = get_user_model().objects.create_user(
+            username="user",
+            password="user12345",
+        )
+        self.client.force_login(self.user)
+
+    def test_login_required(self):
+        res = self.client.get(SKIN_TYPE_LIST_URL)
+
+        self.assertEquals(res.status_code, 200)
