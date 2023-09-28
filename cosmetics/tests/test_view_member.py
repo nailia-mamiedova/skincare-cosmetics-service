4c92bf7f6c7e284a04261daa04f595b946db9e82
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-09-28 11:45:36 +0300

    Create user and login in the setUp method in PrivateMemberListTest

--- cosmetics/tests/test_view_member.py
+++ cosmetics/tests/test_view_member.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
@@ -8,3 +9,11 @@ class PublicMemberListTest(TestCase):
     def test_login_required(self):
         response = self.client.get(MEMBER_LIST_URL)
         self.assertNotEqual(response.status_code, 200)
+
+
+class PrivateMemberListTest(TestCase):
+    def setUp(self) -> None:
+        self.user = get_user_model().objects.create_user(
+            username="user", password="user12345"
+        )
+        self.client.force_login(self.user)
