ab2b263558421811f88fed5907a6b113c45ee16e
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-09-29 16:22:16 +0300

    Configure DEBUG with environment variables & Add localhost to ALLOWED_HOSTS

--- skincare_service/settings.py
+++ skincare_service/settings.py
@@ -26,9 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1"
+]
 
 INTERNAL_IPS = [
     "127.0.0.1",
